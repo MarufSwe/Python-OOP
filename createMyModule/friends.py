38ea888f537a7382316363fe99577952eaa849b3
khanmaruf <khanmaruf.swe@gmail.com>
2020-03-02 00:49:26 +0600

    after create my Module and import from others class

---
+++ createMyModule/friends.py
@@ -0,0 +1,13 @@
+class Friends:
+    Maruf = None
+    Sheiblu = None
+    Mehedi = None
+    Shihab = None
+
+    def set_value(self, Maruf, Sheiblu, Mehedi, Shihab):
+        self.Maruf = Maruf
+        self.Sheiblu = Sheiblu
+        self.Mehedi = Mehedi
+        self.Shihab = Shihab
+
+
